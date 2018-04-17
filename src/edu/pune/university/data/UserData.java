eabc273e4f532af7ec3cd26c4729a8f7551748da
Maya <mayap282@gmail.com>
2018-04-17 23:15:42 +0530

    Water data structure is updated with records.

--- src/edu/pune/university/data/UserData.java
+++ src/edu/pune/university/data/UserData.java
@@ -2,22 +2,25 @@ package edu.pune.university.data;
 
 import org.bson.Document;
 
-public class UserData extends Document{
+public class UserData extends Document {
 	private static final long serialVersionUID = 1L;
-	private String userName ="userName";
-	private String password ="password";
+	private String userName = "userName";
+	private String password = "password";
+
 	public String getUserName() {
 		return userName;
 	}
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
 
 }
--- test/edu/pune/university/dao/WaterDataDaoTest.java
+++ test/edu/pune/university/dao/WaterDataDaoTest.java
@@ -1,5 +1,10 @@
 package edu.pune.university.dao;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import edu.pune.university.data.Record;
 import edu.pune.university.data.WaterData;
 import edu.pune.university.exception.ApplicationException;
 import junit.framework.TestCase;
@@ -20,8 +25,16 @@ public class WaterDataDaoTest extends TestCase {
 
 	public void testCreateWaterDataTest() {
 		WaterData waterData = new WaterData();
-		waterData.setId("4");
-		waterData.setTemperature(41.0F);
+		waterData.setLocation("CTW Dighi Pimpri-Chinchwad, Maharashtra 411039");
+		waterData.setLatitude(18.6123331F);
+		waterData.setLongitude(73.85091160000002F);
+		
+//		List<Record> records = new ArrayList<Record>();
+//		Record record = new Record((new Date()).toString(), 8, 25.0F);
+//		records.add(record);
+//		
+//		waterData.setRecords(records);
+		
 		try {
 			waterDataDao.createWaterData(waterData);
 		} catch (ApplicationException e) {
