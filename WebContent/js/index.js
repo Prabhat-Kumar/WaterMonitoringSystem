8b0e2ba82a0aacdd21aeb05f5e4e5cac1509ad3f
Prabhat <prabhat.kumar@in.ibm.com>
2018-07-10 23:25:07 +0530

    Refresh the google map data.

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -1,6 +1,8 @@
+var markers = [];
 $(document).ready(function(){
 	$("#message").hide();
 	$("#demo").hide();
+	$("#refresh").hide();
     $("#loginSubmit").click(function(event){
     	var postData = {userName : $('#userName').val(), password : $('#password').val()};
     	xhrPost("rest/user/authenticate", postData,  function(result) {
@@ -8,6 +10,7 @@ $(document).ready(function(){
 				$('#loginForm').hide();
 				$("#message").hide();
 				$("#demo").show();
+				$("#refresh").show();
 				showGoogleMap();
 			}else{
 				$('#userName').val("");
@@ -25,12 +28,21 @@ $(document).ready(function(){
     		openGraphDemo("Live Demo from temprature and PH", waterDatas);
     	});
     });
+    $("#refresh").click(function(event){
+    	// Loop through markers and set map to null for each
+        for (var i=0; i<markers.length; i++) {
+            markers[i].setMap(null);
+        }
+        // Reset the markers array
+        markers = [];
+    	showGoogleMap();
+    });
 });
 
 function showGoogleMap() {
 	xhrGet("rest/water/datas", function(waterDatas) {
 		if (waterDatas) {
-			var map = new google.maps.Map(document.getElementById('map'), {
+			map = new google.maps.Map(document.getElementById('map'), {
 			      zoom: 12,
 			      center: new google.maps.LatLng(18.62, 73.65),
 			      mapTypeId: google.maps.MapTypeId.ROADMAP
@@ -52,6 +64,7 @@ function showGoogleMap() {
 			          openGraph (waterDatas[i].location, waterDatas[i].records);
 			        }
 			      })(marker, i));
+			      markers.push(marker);
 			    }
 		}
 	}, function(error, status) {
