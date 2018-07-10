4cf956beaa92b0e568602539d600f313b0851fda
Prabhat <prabhat.kumar@in.ibm.com>
2018-07-10 22:37:36 +0530

    Graph changes

--- WebContent/js/graph.dialog.js
+++ WebContent/js/graph.dialog.js
@@ -19,66 +19,16 @@ function openGraph (titleText, graphData) {
 }
 
 function loadGraph (graphData) {
-	var xAxisLabels = [];
-	var tempratures = [];
-	var phs = [];
-	
-	for (i = 0; i < graphData.length; i++) {  
-		xAxisLabels[i] = graphData[i].time;
-		tempratures[i] = graphData[i].temprature;
-		phs[i] = graphData[i].pH;
-	}
-	var config = {
-		type: 'line',
-		data: {
-			labels: xAxisLabels,
-			datasets: [{
-				label: 'Temprature',
-				backgroundColor: window.chartColors.red,
-				borderColor: window.chartColors.red,
-				data: tempratures,
-				fill: false,
-			}, {
-				label: 'PH',
-				fill: false,
-				backgroundColor: window.chartColors.blue,
-				borderColor: window.chartColors.blue,
-				data: phs,
-			}]
-		},
-		options: {
-			responsive: true,
-			title: {
-				display: true,
-				text: 'Temprature and PH v/s time chart'
-			},
-			tooltips: {
-				mode: 'index',
-				intersect: false,
-			},
-			hover: {
-				mode: 'nearest',
-				intersect: true
-			},
-			scales: {
-				xAxes: [{
-					display: true,
-					scaleLabel: {
-						display: true,
-						labelString: 'Month'
-					}
-				}],
-				yAxes: [{
-					display: true,
-					scaleLabel: {
-						display: true,
-						labelString: 'Temprature and PH'
-					}
-				}]
+
+		var objctx = document.getElementById('canvas').getContext('2d');
+        objctx.beginPath();
+        for (i = 0; i < graphData.length; i++) {
+        	if (i == 0) {
+        		 objctx.moveTo(parseInt(graphData[i].time), graphData[i].pH);
+			} else {
+				objctx.lineTo(parseInt(graphData[i].time), graphData[i].pH);				
 			}
 		}
-	};
-
-		var ctx = document.getElementById('canvas').getContext('2d');
-		window.myLine = new Chart(ctx, config);
+        objctx.closePath();
+        objctx.stroke();
 }
